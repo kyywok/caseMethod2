0b0f96ce160e0746db690b793452ba29e2b2bed2
Aqillah <aqillahzakkyramadana@gmail.com>
2025-12-18 13:12:25 +0700

    cm2

--- CM2Magang04.java
+++ CM2Magang04.java
@@ -24,7 +24,7 @@ public class CM2Magang04 {
                     tampilSemua();
                     break;
                 case 3:
-                    System.out.print("masukkan program studi: ");
+                    System.out.print("masukkan program studi atau nama perusahaan: ");
                     String prodi = sc.next();
                     caripendaftar(prodi);
                     break;
@@ -42,7 +42,7 @@ public class CM2Magang04 {
     // function tambah data
     public static void tambahdata() {
         // input data pendaftar
-        System.out.println("\n--- MASUKKAN DATA ");
+        System.out.println("\n--- MASUKAN DATA ");
         System.out.print("nama: ");
         data[pendaftar][0] = sc.next();
         System.out.print("nim: ");
@@ -84,7 +84,8 @@ public class CM2Magang04 {
 
     // fungsi cari pendaftar
     public static void caripendaftar(String prodi) {
-        System.out.println("\n--- HASIL PENCARIAN PRODI: " + prodi);
+        String namaPerusahaan = prodi;
+        System.out.println("\n--- HASIL PENCARIAN PRODI / NAMA PERUSAHAAN: " + prodi);
 
         // inisialisasi variabel
         boolean ditemukan = false;
@@ -92,6 +93,11 @@ public class CM2Magang04 {
         // perulangan untuk mencari pendaftar yang dicari berdasarkan prodi
         for (int i = 0; i < pendaftar; i++) {
             if (data[i][2].equalsIgnoreCase(prodi)) {
+                System.out.println("nama: " + data[i][0]);
+                System.out.println("nim: " + data[i][1]);
+                System.out.println("status: " + data[i][5]);
+                ditemukan = true;
+            } else if (data[i][3].equalsIgnoreCase(namaPerusahaan)) {
                 System.out.println("nama: " + data[i][0]);
                 System.out.println("nim: " + data[i][1]);
                 System.out.println("status: " + data[i][5]);
@@ -108,7 +114,8 @@ public class CM2Magang04 {
     // fungsi untuk menentukan berapa orang yang diterima, menunggu, dan ditolak
     public static void hitungpendaftar() {
         // inisialisasi variabel
-        int diterima = 0, menunggu = 0, ditolak = 0;
+        int diterima = 0, menunggu = 0, ditolak = 0, total = 0;
+        double presentase = 0;
 
         // perulangan untuk menjulah berapa yang diterima,menunggu, dan ditolak
         for (int i = 0; i < pendaftar; i++) {
@@ -121,10 +128,19 @@ public class CM2Magang04 {
                 ditolak += 1;
             }
         }
+
+        total = diterima + menunggu + ditolak;
+        if (total > 0) {
+            presentase = (double) diterima / total * 100;
+        } else {
+            presentase = 0;
+        }
+
         // tampilkan hasil pendaftar
         System.out.println("\n--- DATA PENDAFTAR YANG DITERIMA/NENUNGGU/DITOLAK");
         System.out.println("diterima: " + diterima);
         System.out.println("menunggu: " + menunggu);
         System.out.println("ditolak: " + ditolak);
+        System.out.println("presentase kelulusan (diterima) dari semua pendaftar: " + presentase);
     }
 }
